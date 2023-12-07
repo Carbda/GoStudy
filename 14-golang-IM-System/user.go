6ee0f2326d4b6a621078f675ac70e7b499b56cfe
wangyh <740458268@qq.com>
2023-12-07 17:35:05 +0800

    周四提交

--- 14-golang-IM-System/user.go
+++ 14-golang-IM-System/user.go
@@ -1,6 +1,9 @@
 package main
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 type User struct {
 	Name string
@@ -37,7 +40,7 @@ func (u *User) Online() {
 	u.server.mapLock.Unlock()
 
 	//广播当前用户上线消息
-	u.server.BroadCast(u, "online")
+	u.server.BroadCast(u, "online!")
 }
 
 // 用户的下线业务
@@ -48,12 +51,43 @@ func (u *User) Offline() {
 	u.server.mapLock.Unlock()
 
 	//广播当前用户下线消息
-	u.server.BroadCast(u, "offline")
+	u.server.BroadCast(u, "offline!")
+}
+
+// 给当前User对应客户端发送消息
+func (u *User) SendMsg(msg string) {
+	u.Conn.Write([]byte(msg))
 }
 
 // 用户处理消息的业务
 func (u *User) DoMessage(msg string) {
-	u.server.BroadCast(u, msg)
+	if msg == "who" {
+		//查询当前在线用户有哪些
+		u.server.mapLock.Lock()
+		for _, user := range u.server.OnlineMap {
+			onlineMsg := "[" + user.Addr + "]" + user.Name + ":" + "is online\n"
+			u.SendMsg(onlineMsg)
+		}
+		u.server.mapLock.Unlock()
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		//消息格式：rename|张三
+		newName := strings.Split(msg, "|")[1]
+		//判断name是否存在
+		_, ok := u.server.OnlineMap[newName]
+		if ok {
+			u.SendMsg("name is used!\n")
+		} else {
+			u.server.mapLock.Lock()
+			delete(u.server.OnlineMap, u.Name)
+			u.server.OnlineMap[newName] = u
+			u.server.mapLock.Unlock()
+
+			u.Name = newName
+			u.SendMsg("update newName:" + newName + "\n")
+		}
+	} else {
+		u.server.BroadCast(u, msg)
+	}
 }
 
 // 监听当前User channel的方法，一旦有消息就发送给对端客户端
